1280e4cf358641503203fc71c0227a5a119bc109
Mohit Kumar <mohitahlawat912@gmail.com>
2025-08-14 14:44:16 +0530

    debouncing done

--- Week-9/Class 9.1/custom-hook-5/src/App.jsx
+++ Week-9/Class 9.1/custom-hook-5/src/App.jsx
@@ -1,35 +1,34 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-const useDebounce = (value, delay) => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+const App = () => {
+  const [name, setName] = useState("");
 
   useEffect(() => {
-    const timerId = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
+    // set a timeout when name changes
+  let timerId=setTimeout(() => {
+        console.log(`You have searched for ${name}`);
+    }, 600);
 
-    // Clean up the timer if the value changes before the delay has passed
-    return () => clearTimeout(timerId);
-  }, [value, delay]);
+    // cleanup function to clear timeout on next name change
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [name]);
 
-  return debouncedValue;
-};
-
-
-const SearchBar = () => {
-  const [inputValue, setInputValue] = useState('');
-  const debouncedValue = useDebounce(inputValue, 2000);
-  
   return (
-    <>
-    Debounced value is {debouncedValue}
-    <input type="text"  onChange={(e) => setInputValue(e.target.value)} placeholder="Search..."/>
-    </>
-  
+    <div>
+      <input
+        type="text"
+        placeholder="What do you want to search?"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </div>
   );
 };
 
-export default SearchBar;
+export default App;
+
 
 // Debouncing is a programming practice used to ensure that time-consuming tasks do not fire so often, making them 
 // more efficient. When a user types quickly, a debouncing mechanism introduces a delay before the associated event
